2a5da6af1241ef9005efd2c1aa7374b3cb93921c
laksh <laksh@LAKSH>
2026-09-24 16:26:06 +0530

    Add project files.

---
+++ Azure-App-Learn1/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Azure_App_Learn1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
